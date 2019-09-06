f0318e4629d78f4e7d6f8bc8c1c8f5e222f14298
Edwin <46379855+Ebi2626@users.noreply.github.com>
2019-09-06 21:43:49 +0200

    First day of work
    It contains:
    - first sketch of mysql database
    - first unfinshed sketch of homepage
    - webpack configuration
    - a piece of sass code
    - images

---
+++ index.php
@@ -0,0 +1,201 @@
+<!DOCTYPE html>
+<html lang="pl">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Stronnica - księgarnia internetowa</title>
+    <meta name="description" content="Stronnica to pierwsza w Polsce księgarnia internetowa z możliwością...">
+    <link rel="stylesheet" href="style.bundle.css">
+</head>
+<body>
+<header class="mainHeader">
+    <nav class="menu">
+        <ul class="menu__left">
+            <li class="menu__item"><a href="" class="menu__link">Ksiązki</a></li>
+            <li class="menu__item"><a href="" class="menu__link">Autorzy</a></li>
+            <li class="menu__item"><a href="" class="menu__link">Kategorie</a></li>
+        </ul>
+        <div class="menu__logo"><img src="./img/logo.svg" alt="logo" class="logo__img"></div>
+        <ul class="menu__right">
+            <li class="menu__item"><a href="" class="menu__link">Zaloguj się</a></li>
+            <li class="menu__item"><a href="" class="menu__link">Zarejestruj się</a></li>
+            <li class="menu__item"><a href="" class="menu__link">Ikona</a></li>
+        </ul>
+    </nav>
+        <div class="mainHeader__box">
+            <p class="mainHeader__smallText">Cała potęga wiedzy</p>
+            <h3 class="mainHeader__title">Tania literatura. <br /> Bezcenna wiedza.</h3>
+            <p class="mainHeader__text">Wierzymy, że to właśnie wiedza jest tym, co pozwoliło człowiekowi wyewoluować i przetrwać tyle setek tysięcy lat. Dzisiaj, w cywyilizacji wysokich technologii wiedza jest podstawowym fundamentem nie tylko sukcesu życiowego, ale w ogóle skutecznego funkcjonowania. Dzięki wiedzy zawartej w książkach naszej księgarni każdy człowiek może opanować najbardziej skomplikowane mechanizmy i czuć się w naszej cyfrowej rzeczywistości, jak ryba w wodzie.</p>
+        </div>
+    </header>
+    <div class="category">
+        <ul class="category__list">
+            <li class="category__item"><a href="" class="category__link">Informatyka</a></li>
+            <li class="category__item"><a href="" class="category__link">Matematyka</a></li>
+            <li class="category__item"><a href="" class="category__link">Fizyka</a></li>
+            <li class="category__item"><a href="" class="category__link">Inżynieria</a></li>
+        </ul>
+    </div>
+    <section class="it">
+        <div class="header">
+            <div class="titleBox">
+                <h5 class="title">Informatyka</h5>
+                <p class="subtitle">Programowanie. Konfiguracja systemów operacyjnych. Urządzenia.</p>
+                <div class="button">
+                    <p class="buttonContent">Więcej na temat informatyki</p>
+                </div>
+            </div>
+        </div>
+        <div class="content">
+            <div class="contentHeader">
+                <h5 class="contentTitle">Informatyka</h5>
+                <p class="contentText">Podręczniki od 29,99zł</p>
+            </div>
+            <div class="contentBox">
+                <div class="content__items" style="backround-color: rgba(0, 0, 0, .5);"></div>
+                <div class="content__items" style="backround-color: rgba(0, 0, 0, .5);"></div>
+                <div class="content__items" style="backround-color: rgba(0, 0, 0, .5);"></div>
+                <div class="content__items" style="backround-color: rgba(0, 0, 0, .5);"></div>
+            </div>
+        </div>
+    </section>
+    <section class="math">
+        <div class="header">
+            <div class="titleBox">
+                <h5 class="title">Matematyka</h5>
+                <p class="subtitle">Równania. Całki. Macierze. Repozytoria. Algorytmika.</p>
+                <div class="button">
+                    <p class="buttonContent">Więcej na temat matematyki</p>
+                </div>
+            </div>
+        </div>
+        <div class="content">
+            <div class="contentHeader">
+                <h5 class="contentTitle">Matematyka</h5>
+                <p class="contentText">Podręczniki od 19,99zł</p>
+            </div>
+            <div class="contentBox">
+            <div class="content__items" style="backround-color: rgba(0, 0, 0, .5);"></div>
+                <div class="content__items" style="backround-color: rgba(0, 0, 0, .5);"></div>
+                <div class="content__items" style="backround-color: rgba(0, 0, 0, .5);"></div>
+                <div class="content__items" style="backround-color: rgba(0, 0, 0, .5);"></div>
+            </div>
+        </div>
+    </section>
+      <section class="physics">
+        <div class="header">
+            <div class="titleBox">
+                <h5 class="title">Fizyka</h5>
+                <p class="subtitle">Newtonowska i kwantowa. Repozytoria. Zbiory zadań.</p>
+                <div class="button">
+                    <p class="buttonContent">Więcej na temat fizyki</p>
+                </div>
+            </div>
+        </div>
+        <div class="content">
+            <div class="contentHeader">
+                <h5 class="contentTitle">Fizyka</h5>
+                <p class="contentText">Podręczniki od 25,99zł</p>
+            </div>
+            <div class="contentBox">
+            <div class="content__items" style="backround-color: rgba(0, 0, 0, .5);"></div>
+                <div class="content__items" style="backround-color: rgba(0, 0, 0, .5);"></div>
+                <div class="content__items" style="backround-color: rgba(0, 0, 0, .5);"></div>
+                <div class="content__items" style="backround-color: rgba(0, 0, 0, .5);"></div>
+            </div>
+        </div>
+    </section>
+    <section class="enginering">
+        <div class="header">
+            <div class="titleBox">
+                <h5 class="title">Inżynieria</h5>
+                <p class="subtitle">Inżynieria oprogramowania. Inżynieria materiałowa. Robotyka. Teleinformatyka.</p>
+                <div class="button">
+                    <p class="buttonContent">Więcej na tematy techniczne</p>
+                </div>
+            </div>
+        </div>
+        <div class="content">
+            <div class="contentHeader">
+                <h5 class="contentTitle">Inżynieria</h5>
+                <p class="contentText">Podręczniki od 49,99zł</p>
+            </div>
+            <div class="contentBox">
+            <div class="content__items" style="backround-color: rgba(0, 0, 0, .5);"></div>
+                <div class="content__items" style="backround-color: rgba(0, 0, 0, .5);"></div>
+                <div class="content__items" style="backround-color: rgba(0, 0, 0, .5);"></div>
+                <div class="content__items" style="backround-color: rgba(0, 0, 0, .5);"></div>
+            </div>
+        </div>
+    </section>
+    <div class="banner">
+        <div class="banner__content">
+            <img src="./img/ml.jpg" alt="matematyka" class="banner__img">
+            <img src="./img/el.jpg" alt="inżynieria" class="banner__img">
+            <img src="./img/il.jpg" alt="informatyka" class="banner__img">
+            <img src="./img/fl.jpg" alt="fizyka" class="banner__img">
+        </div>
+        <div class="banner__content">
+            <div class="banner__header">
+            <p class="banner__subtitle">#Stronnica</p>
+            <h5 class="banner__title">Dzielimy się wiedzą.</h5>
+                <div class="banner__button">
+                 <p class="banner__buttonContent">Śledź nas na twitterze</p>
+                </div>
+            </div>
+         </div>
+        <div class="banner__content">
+        <img src="./img/mr.jpg" alt="matematyka" class="banner__img">
+            <img src="./img/er.jpg" alt="inżynieria" class="banner__img">
+            <img src="./img/ir.jpg" alt="informatyka" class="banner__img">
+            <img src="./img/fr.png" alt="fizyka" class="banner__img">
+        </div>
+    </div>
+    <div class="newsletter">
+        <div class="newsletter__header">
+            <h6 class="newsletter__title">Zapisz się na nasz newsletter.</h6>
+            <p class="newsletter__subtitle">Bądź na bieżąco z najnowszymi dokonaniami naukowymi i promocjami.</p>
+        </div>
+        <form class="newsletter__saver">
+            <p class="newsletter__saverText">Wpisz swój adres email poniżej</p>
+            <input type="text" class="newsletter__input" placeholder="Twój email">
+            <input type="submit" class="newsletter__input" value="Wyślij">
+        </form>
+    </div>
+    <footer class="footer">
+        <div class="footer__logo"><img src="" alt="" class="footer__img"></div>
+        <div class="footer__content">
+            <ul class="footer__list">
+                <li class="footer__listHeader">Książki</li>
+                <li class="footer__link"><a href=""></a></li>
+                <li class="footer__link"><a href=""></a></li>
+                <li class="footer__link"><a href=""></a></li>
+                <li class="footer__link"><a href=""></a></li>
+            </ul>
+            <ul class="footer__list">
+                <li class="footer__listHeader">O nas</li>
+                <li class="footer__link"><a href=""></a></li>
+                <li class="footer__link"><a href=""></a></li>
+                <li class="footer__link"><a href=""></a></li>
+            </ul>
+            <ul class="footer__list">
+                <li class="footer__listHeader">Kontakt</li>
+                <li class="footer__link"><a href=""></a></li>
+                <li class="footer__link"><a href=""></a></li>
+                <li class="footer__link"><a href=""></a></li>
+            </ul>
+        </div>
+    </footer>
+    <div class="contact">
+        <ul class="contact__list">
+            <li class="contact__item">2019</li>
+            <li class="contact__item">Stronnica.</li>
+            <li class="contact__item">Polityka prywatności</li>
+            <li class="contact__item">Twitter</li>
+            <li class="contact__item">Facebook</li>
+        </ul>
+    </div>
+<script src="main.bundle.js"></script>
+</body>
+</html>
